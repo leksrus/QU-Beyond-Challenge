90483bc00326710785d1d9fb164bb20d9abd5dc3
Aleksey Kotylev <aleks_rus@hotmail.com>
2020-03-06 14:46:59 -0300

    Initial SetUp

---
+++ QU-Beyond-Challenge/BLL/WordFinder.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class WordFinder : IWordFinder
+    {
+        private readonly IEnumerable<string> _matrix;
+
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            _matrix = matrix;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QU-Beyond-Challenge/QU-Beyond-Challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QU_Beyond_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
