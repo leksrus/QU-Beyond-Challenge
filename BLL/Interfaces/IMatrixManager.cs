95d423fbc8aadc48135a0c95166300e67c968733
Aleksey Kotylev <aleks_rus@hotmail.com>
2020-03-09 17:03:44 -0300

    File relocation

---
+++ BLL/Interfaces/IMatrixManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BLL.Interfaces
+{
+    public interface IMatrixManager
+    {
+        char[,] GetMatrix(IEnumerable<string> matrix);
+
+        bool SearchWordExistence(char[,] grid, int row, int col, string word);
+    }
+}
---
+++ BLL/Interfaces/IWordFinder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BLL.Interfaces
+{
+    public interface IWordFinder
+    {
+        IEnumerable<string> Find(IEnumerable<string> wordStream);
+    }
+}
